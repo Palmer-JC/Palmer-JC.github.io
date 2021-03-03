d6ee1cb71fa0f89649e0cbc4bc919a2353cbbc37
Palmer-JC <jeffrey.c.palmer@gmail.com>
2021-03-03 12:01:22 -0500

    clean up report for sliders which also enable

--- scenes/sandboxXR/materialsTab.js
+++ scenes/sandboxXR/materialsTab.js
@@ -26,19 +26,25 @@ class MatSettings {
         let ret = '';
         if (this.material.environmentIntensity !== this.environmentIntensity) ret += `\n\tEnv intensity: ${this.material.environmentIntensity}`;
 
-        if (this.material.subSurface.isTranslucencyEnabled !== this.subSurfaceIsTranslucencyEnabled) ret += `\n\tSubsurf Translucency enabled: ${this.material.subSurface.isTranslucencyEnabled}`;
-        if (this.material.subSurface.translucencyIntensity !== this.subSurfaceTranslucencyIntensity) ret += `\n\tSubsurf Translucency intensity: ${this.material.subSurface.translucencyIntensity}`;
-        if (!this.material.subSurface.tintColor.equals(this.subSurfaceTintColor)) ret += `\n\tSubsurf Translucency tint color: ${this.material.subSurface.tintColor.toString()}`;
+        if (this.material.subSurface.isTranslucencyEnabled !== this.subSurfaceIsTranslucencyEnabled) {
+            ret += `\n\tSubsurf Translucency enabled: ${this.material.subSurface.isTranslucencyEnabled}`;
+            if (this.material.subSurface.translucencyIntensity !== this.subSurfaceTranslucencyIntensity) ret += `\n\tSubsurf Translucency intensity: ${this.material.subSurface.translucencyIntensity}`;
+            if (!this.material.subSurface.tintColor.equals(this.subSurfaceTintColor)) ret += `\n\tSubsurf Translucency tint color: ${this.material.subSurface.tintColor.toString()}`;
+        }
 
         if (!this.material.albedoColor.equals(this.albedoColor)) ret += `\n\tAlbedo color: ${this.material.albedoColor.toString()}`;
         if (this.material.metallic !== this.metallic) ret += `\n\tMetallic: ${this.material.metallic}`;
         if (this.material.roughness !== this.roughness) ret += `\n\tRoughness: ${this.material.roughness}`;
 
-        if (this.material.clearCoat.isEnabled !== this.clearCoatIsEnabled) ret += `\n\tClearcoat enabled: ${this.material.clearCoat.isEnabled}`;
-        if (this.material.clearCoat.intensity !== this.clearCoatIntensity) ret += `\n\tClearcoat intensity: ${this.material.clearCoat.intensity}`;
+        if (this.material.clearCoat.isEnabled !== this.clearCoatIsEnabled) {
+            ret += `\n\tClearcoat enabled: ${this.material.clearCoat.isEnabled}`;
+            if (this.material.clearCoat.intensity !== this.clearCoatIntensity) ret += `\n\tClearcoat intensity: ${this.material.clearCoat.intensity}`;
+        }
 
-        if (this.material.sheen.isEnabled !== this.sheenIsEnabled) ret += `\n\tSheen enabled: ${this.material.sheen.isEnabled}`;
-        if (this.material.sheen.intensity !== this.sheenIntensity) ret += `\n\tSheen intensity: ${this.material.sheen.intensity}`;
+        if (this.material.sheen.isEnabled !== this.sheenIsEnabled) {
+            ret += `\n\tSheen enabled: ${this.material.sheen.isEnabled}`;
+            if (this.material.sheen.intensity !== this.sheenIntensity) ret += `\n\tSheen intensity: ${this.material.sheen.intensity}`;
+        }
 
         return `${this.material.name}: ${(ret.length > 0) ? ret : '  NONE'}`;
 
